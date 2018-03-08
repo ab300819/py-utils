23ed0539df1cc321a9ea2c5f492fce8ca70c9245
Mason <bigger831>
2018-03-08 17:57:53 +0800

    add numpy pracrise

--- utils/analysis_log.py
+++ utils/analysis_log.py
@@ -3,10 +3,15 @@
 import sys
 
 print('参数个数：%d' % len(sys.argv))
-print('参数名称：%s' % sys.argv[1])
+# print('参数名称：%s' % sys.argv[1])
 
-def main():
-    pass
+
+def read_file(file_path):
+    with open(file_path, 'r') as f:
+        f.read()
+
 
 if __name__ == '__main__':
-    main()
+    match_rule = r'^[response|response]\(([0-9a-zA-Z]{32})\)$'
+    file_path = 'C:\\Users\\lenovo\\Desktop\\application_external.log.2018-03-07.4'
+    read_file(file_path)
