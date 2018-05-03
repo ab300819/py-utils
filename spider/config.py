5dd3e27809f14431fc241a58b5c44f706e2c1791
Mason <ab300819@163.com>
2018-05-04 00:39:15 +0800

    update grab_zhihu.py

---
+++ spider/config.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+ZHIHU_MAIN_URL = 'https://www.zhihu.com'
+
+CHROME_AGENT = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'
+}
