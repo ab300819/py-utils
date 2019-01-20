148d99bf2b3449cd0ff59a031b479a3c4c6d2236
Mason <ab300819@163.com>
2019-01-21 01:05:29 +0800

    添加视频爬爬虫

--- spider/grab_video.py
+++ spider/grab_video.py
@@ -1,5 +1,7 @@
+import mysql.connector as database
 import requests as rqs
 from pyquery import PyQuery as pq
+import time
 
 '''
                    _ooOoo_
@@ -24,14 +26,64 @@ from pyquery import PyQuery as pq
              佛祖保佑     永无BUG
 '''
 
-url = 'http://www.888tv.co/video/890/%E7%A5%9E%E7%94%B0%E3%82%8B%E3%81%AA-%E3%81%8B%E3%82%93%E3%81%A0%E3%82%8B%E3%81%AA-%E7%B6%9A%E3%80%85%E7%94%9F%E4%B8%AD-%E5%85%83%E3%83%A9%E3%82%A6%E3%83%B3%E3%83%89%E3%82%AC%E3%83%BC%E3%83%AB%E3%82%92%E3%82%A4%E3%82%AB%E3%81%9B%E3%81%BE%E3%81%8F%E3%82%8B-%E7%A5%9E%E7%94%B0%E3%82%8B%E3%81%AA-1691'
-user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'
-header = {'user-agent': user_agent}
+MAIN_URL = 'http://www.888tv.co/videos?page='
+TOTAL_PAGE = 45
+START_PAGE = 1
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'
+HEADER = {'user-agent': USER_AGENT}
 
-html_source = rqs.get(url, headers=header)
-print(html_source.text)
+# SQL
+INSERT_VIDEO_LIST = "INSERT INTO video_list(title,url,rate,download_status) values (%s,%s,%s,%s)"
+UPDATE_VIDEO_LIST = ''
+DELETE_VIDEO_LIST = ''
+SELECT_VIDEO_LIST = ''
 
-html = pq(html_source.text)
-print(html)
 
-video_source = html('source')
+def get_html_response(url):
+    html_source = rqs.get(MAIN_URL, headers=HEADER)
+    return html_source.text
+
+
+def get_video_list(html):
+    video_list_result = []
+    html_parse = pq(html)
+    video_list = html_parse('.col-sm-6.col-md-4.col-lg-4')
+
+    for i in video_list:
+        single_video = []
+        video = pq(i)
+        single_video.append(video('span').text())
+        single_video.append(video('a').attr('href'))
+        single_video.append(remove_str(video('b').text(), '%'))
+        single_video.append(0)
+        print(single_video)
+        video_list_result.append(single_video)
+    return video_list_result
+
+
+def get_video_file(html):
+    html_parse = pq(html)
+
+
+def remove_str(source, target):
+    return source.rstrip(target)
+
+
+if __name__ == '__main__':
+
+    all_video = []
+
+    for i in range(START_PAGE, TOTAL_PAGE+1):
+        html_res = get_html_response(MAIN_URL + str(i))
+        video_result = get_video_list(html_res)
+        all_video.extend(video_result)
+        print(i)
+        time.sleep(9)
+
+    connect = database.connect(host='192.168.9.109', user='root', password='110119', database='test')
+    cursor = connect.cursor()
+    for value in all_video:
+        cursor.execute(INSERT_VIDEO_LIST, value)
+    print(cursor.rowcount)
+    connect.commit()
+    connect.close()
