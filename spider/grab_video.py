61ce6843a685319af13b2766be70a956c314e6bb
Mason <ab300819@163.com>
2019-01-23 00:54:42 +0800

    更新逻辑

--- spider/grab_video.py
+++ spider/grab_video.py
@@ -1,4 +1,6 @@
+import re
 import time
+import xmlrpc.client as rpc
 
 import mysql.connector as database
 import requests as rqs
@@ -28,8 +30,9 @@ from pyquery import PyQuery as pq
              佛祖保佑     永无BUG
 '''
 
+PREFIX = 'http:'
 MAIN_URL = 'http://www.888tv.co/videos?page='
-TOTAL_PAGE = 45
+TOTAL_PAGE = 46
 START_PAGE = 1
 SLEEP_TIME = 9
 USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'
@@ -37,11 +40,19 @@ US_TAG = 'HD_US'
 CN_TAG = 'HD_CN'
 HEADER = {'user-agent': USER_AGENT}
 
+REMOTE = 'localhost'
+ARIA2_URL = 'http://' + REMOTE + ':6800/rpc'
+ARIA2_TOKEN = 'token:a6516320-9d0f-48dc-bf56-2cdd5314e131'
+FILE_PATH = '/home/think/Samba/downloads/'
+ARIA2_DIR = 'dir'
+
 # SQL
 INSERT_VIDEO_LIST = "INSERT INTO video_list(title,url,rate,download_status) values (%s,%s,%s,%s)"
 UPDATE_VIDEO_LIST = ''
 DELETE_VIDEO_LIST = ''
 SELECT_VIDEO_LIST = ''
+
+VIDEO_URL_RULE = re.compile('^[http|//].+video\d+\.ts$')
 
 
 def get_html_response(url):
@@ -69,12 +80,39 @@ def get_video_list(html):
 def get_video_file(html):
     html_parse = pq(html)
     video_source_list = html_parse('source')
-    video_source = ''
+
+    if len(video_source_list) == 0:
+        element = pq(video_source_list[0])
+        return element.attr('src')
+
     for i in video_source_list:
         element = pq(i)
         if element.attr('label') is CN_TAG:
-            video_source = element.attr('src')
-            break
+            return element.attr('src')
+
+
+def get_video_file_url(url):
+    video_url_list = []
+
+    if PREFIX in url:
+        url = PREFIX + url
+    video_url_request = get_html_response(url)
+    video_list = video_url_request.split('\n')
+    if video_list:
+        video_url_list = [x for x in video_list if VIDEO_URL_RULE.match(x)]
+
+    return [PREFIX + x for x in video_url_list if PREFIX not in x]
+
+
+def download_video_list(title, url_List):
+    result_list = []
+
+    server = rpc.ServerProxy(ARIA2_URL)
+    if url_List:
+        for i in url_List:
+            result = server.aria2.addUri(ARIA2_TOKEN, [i], {ARIA2_DIR: FILE_PATH + title})
+            result_list.append(result)
+    return result_list
 
 
 def remove_str(source, target):
@@ -102,9 +140,7 @@ if __name__ == '__main__':
         print(i)
         try:
             cursor.executemany(INSERT_VIDEO_LIST, video_result)
-        except IntegrityError:
-            print("重复")
-        except InterfaceError:
+        except (IntegrityError, InterfaceError):
             print("重复")
         connect.commit()
         time.sleep(SLEEP_TIME)
