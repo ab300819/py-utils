eedb3e2af6bf2fa671cd6e9923d2041baaaa4732
mason <ab300819@163.com>
2020-05-12 20:59:26 +0800

    feat: 递归获取 zk 节点信息

--- utils/zk_viewer.py
+++ utils/zk_viewer.py
@@ -1,23 +1,69 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import logging
-import sys
+__author__ = 'mason'
 
 from kazoo.client import KazooClient
 
-logging.basicConfig(level=logging.DEBUG, stream=sys.stdout,
-                    format='%(asctime)s %(funcName)s%(lineno)d %(levelname)s: %(message)s')
 
-zk = KazooClient(hosts='', timeout=10, logger=logging)
+class ZkNode(object):
+    def __init__(self, path, data, stat):
+        if path == '':
+            self.__node = path
+        elif path == '/':
+            self.__node = path
+        else:
+            self.__node = path.split('/')[-1]
+        self.__path = path
+        self.__data = data
+        self.__stat = stat
+        self.__child = []
+
+    def get_node(self):
+        return self.__node
+
+    def set_child(self, child):
+        self.__child.append(child)
+
+    def get_data(self):
+        return self.__data
+
+    def get_stat(self):
+        return self.__stat
+
+    def get_child(self):
+        return self.__child
+
+
+def zk_walk(zk, node, node_info, level=0):
+    data, stat = zk.get(node)
+    current_node = ZkNode(node, data.decode('utf-8'), stat)
+    node_info.set_child(current_node)
+    children = zk.get_children(node)
+    if len(children) > 0:
+        for sub in children:
+            sub_node = node + '/' + sub if node != '/' else '/' + sub
+            zk_walk(zk, sub_node, current_node, level + 1)
+
+
+def print_node(node, level):
+    if level == 0:
+        print('/')
+    path_list = node.split('/')
+    if len(path_list) < 1:
+        return
+    path = ''
+    for i in range(0, level):
+        path += '-'
+    path = path + path_list[-1]
+    print(path)
+
+
+zk = KazooClient(hosts='', timeout=10)
 zk.start()
 
-data, stat = zk.get('/')
-print(data)
-print(stat)
-
-children = zk.get_children('/')
-print(children)
+root_node = ZkNode('', '', '')
+zk_walk(zk, '/', root_node)
 
 zk.stop()
 zk.close()
