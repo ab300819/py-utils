24aa00d39946bc6f009b9ff8c7548bcd603e4f14
mason <ab300819@163.com>
2020-05-12 15:31:37 +0800

    feat: 添加 zk 连接

--- utils/zk_viewer.py
+++ utils/zk_viewer.py
@@ -1,2 +1,23 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+import logging
+import sys
+
+from kazoo.client import KazooClient
+
+logging.basicConfig(level=logging.DEBUG, stream=sys.stdout,
+                    format='%(asctime)s %(funcName)s%(lineno)d %(levelname)s: %(message)s')
+
+zk = KazooClient(hosts='', timeout=10, logger=logging)
+zk.start()
+
+data, stat = zk.get('/')
+print(data)
+print(stat)
+
+children = zk.get_children('/')
+print(children)
+
+zk.stop()
+zk.close()
