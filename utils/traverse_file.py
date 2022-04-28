f362d78c88abc54741f34fe1cb91a31bd2bf192f
Mason <ab300819@163.com>
2022-04-28 13:32:09 +0800

    feat: 遍历问价夹并计算 md5

---
+++ utils/traverse_file.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+__author__ = 'mason'
+
+import hashlib as hash
+import os
+import sys
+
+"""
+遍历文件夹内所有文件，并计算 md5
+"""
+
+
+def find_all_file(root):
+    for root, dir_list, file_list in os.walk(root):
+        for f in file_list:
+            full_name = os.path.join(root, f)
+            yield full_name
+
+
+def check_md5(file_name: str):
+    with open(file_name, 'rb') as f:
+        md5 = hash.md5()
+        md5.update(f.read())
+        mdd_hash = str(md5.hexdigest())
+    return mdd_hash
+
+
+if __name__ == '__main__':
+    root_path = sys.argv[1]
+    result = []
+    for full_file_name in find_all_file(root_path):
+        md5_sum = ["'" + full_file_name + "'", check_md5(full_file_name)]
+        result.append(md5_sum)
+    with open(root_path.strip(os.sep) + '_file_md5', 'w') as f:
+        for file in result:
+            cell = ','.join(file) + '\n'
+            f.write(cell)
